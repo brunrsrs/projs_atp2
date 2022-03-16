95457d26caf74552382e47dc0b30f36a2fc201ca
Bruno Silveira <93217089+brunrsrs@users.noreply.github.com>
2022-03-16 16:29:16 -0300

    Update Projeto1_Bruno_Silveira.c

--- Projeto1_Bruno_Silveira.c
+++ Projeto1_Bruno_Silveira.c
@@ -2,31 +2,31 @@
 
 int main()  {
 FILE *entrada, *saida;
-int n, k;   //Sendo n: largura da imagem (em pixeis); k: altura da imagem (em pixeis)
-int R, G, B;    //Sendo RGB as cores do pixel da imagem (red, green, blue)
+int n, k;   //n: "image's width" (in pixels); k: "image's length" (im pixels)
+int R, G, B;    //RGB are the colours (red, green, blue)
 
 entrada = fopen ("figura.dat", "r");
 saida = fopen ("cinza.dat", "w");
 
-    if (entrada == NULL)    //Verificando se ha o arquivo "figura.dat"
+    if (entrada == NULL)    //Verifying if "figura.dat" exists
         printf ("\n\tNao foi encontrado \"figura.dat\" para ser aberto\n");
     
     else    {
-        printf ("\n\tArquivo aberto e criado com sucesso\n");   //Resposta ao usuario
+        printf ("\n\tArquivo aberto e criado com sucesso\n");   //Answering the user
 
         fscanf(entrada,"%d %d\n", &n, &k);
-        fprintf (saida,"%d %d\n", n, k);    //Forma a primeira linha mostrando largura/altura
+        fprintf (saida,"%d %d\n", n, k);    //Shows width and length on "cinza.dat"
 
         for (int aux_linha=0; aux_linha<k; aux_linha++)   {
             for (int aux_coluna=0; aux_coluna<n; aux_coluna++) {
                 fscanf(entrada,"%d %d %d ", &R, &G, &B);
-                fprintf (saida, "%d ", (R+G+B)/3);  //Mostra os valores em escalas de cinza
+                fprintf (saida, "%d ", (R+G+B)/3);  //Converts the RBG image to a grayscale
             }
-        fprintf (saida, "\n");  //Passa para a proxima linha do arquivo de saida
+        fprintf (saida, "\n");  //Jumps to the next line
         }
     }
 
-fclose(entrada);    //Garantir que o arquivo será encerrado
+fclose(entrada);    //Garantee the archive will be closed
 fclose(saida);
 
 return 0;
